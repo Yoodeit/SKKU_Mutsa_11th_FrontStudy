6579d1fae7a4167df65310434eddee3214f5e6b0
Yoodeit <juno8147@gmail.com>
2023-05-29 20:28:46 +0900

    6th week preparation2

--- 6th_week/react-for-beginners/src/App.js
+++ 6th_week/react-for-beginners/src/App.js
@@ -1,9 +1,30 @@
+/*
+import Button from "./Button"; 
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1 className={styles.title}>Welcome back!</h1>
+      <Button text={"Continue"} />
     </div>
   );
+}
+
+export default App;
+*/
+import { useState } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("call an api");
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  )
 }
 
 export default App;
